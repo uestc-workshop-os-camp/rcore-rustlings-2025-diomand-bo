e8cbdc8f22800ad1b64e662cb1c5962a766ade38
Diomand-Bo <3317958211@qq.com>
2025-09-23 15:04:35 +0800

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -13,7 +13,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word)= optional_target {
             assert_eq!(word, target);
         }
     }
